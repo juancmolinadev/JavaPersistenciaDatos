bba8744ee1778e0c37060063f4fb990a39d74ef3
Juan Camilo Molina <juanc.molinadev@gmail.com>
2024-04-26 14:09:20 -0500

    first commit

---
+++ src/main/java/app/personalprojects/mensajes_app/Mensajes_app.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package app.personalprojects.mensajes_app;
+
+/**
+ *
+ * @author MolinAnimation
+ */
+public class Mensajes_app {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
